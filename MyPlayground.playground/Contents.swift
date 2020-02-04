ed2be6630f28e997681a6ed14d29497e3c776651
CM Student <cmstudent@uhwo-acm-c-14.local>
2020-02-04 10:52:47 -1000

    Part 5

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -10,6 +10,8 @@ if goodGrade == "I've been doing all my work"
 
 }
 else
+    
+    
 {
     print ("I need to work harder in this class")
     
@@ -17,7 +19,7 @@ else
 
 var gradeA = true
     
-if (gradeA)
+if gradeA == true
 {
   print("I really like this class")
 }
@@ -25,3 +27,24 @@ else
 {
     print("I'm not sure about coding")
 }
+
+
+var integer = 1
+var integerone = 2
+
+if integerone < integer
+{
+    print("I need sleep")
+}
+else
+{
+    print("I don't need sleep")
+}
+
+
+var las = "I like apples"
+var lasone = "I hate apples"
+var lastwo = lasone + las
+
+
+
